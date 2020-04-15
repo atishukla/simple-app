16829f86cc5668dc02da1970fec29f627bdbfa36
atishukla <atishayshukla@gmail.com>
2020-04-15 22:42:27 +0200

    multiline variable

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -7,9 +7,6 @@ SHORT_HASH=$(echo $COMMIT_SHA1 | cut -c -7)
 
 # We must export it so it's available for envsubst
 export SHORT_HASH=$SHORT_HASH
-# export KUBERNETES_CLUSTER_CERTIFICATE=$KUBERNETES_CLUSTER_CERTIFICATE
-# export KUBERNETES_SERVER=$KUBERNETES_SERVER
-# export KUBERNETES_TOKEN=$KUBERNETES_TOKEN
 
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
@@ -18,7 +15,7 @@ envsubst <./kube/01-simple-app-deployment.yaml >./kube/01-simple-app-deployment.
 mv ./kube/01-simple-app-deployment.yaml.out ./kube/01-simple-app-deployment.yaml
 
 
-# echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --wrap=0 | base64 --decode > cert.crt
 
 ./kubectl \
   --kubeconfig=/dev/null \
